764ce4ff57e1d4130e568d244f6041115d3a79fa
Ganesh Patil <ganeshpatil0101@gmail.com>
2020-04-12 11:28:40 +0530

    removed creds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCgEBss0Xmi7PHQL8allUCMIh7LHiXnxZs",
-    authDomain: "my-movies-5792e.firebaseapp.com",
-    databaseURL: "https://my-movies-5792e.firebaseio.com",
-    projectId: "my-movies-5792e",
-    storageBucket: "my-movies-5792e.appspot.com",
-    messagingSenderId: "694551446389",
-    appId: "1:694551446389:web:e34fab5e40eb72d73b093c",
-    measurementId: "G-HVGQQ92SXJ"
+    
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCgEBss0Xmi7PHQL8allUCMIh7LHiXnxZs",
-    authDomain: "my-movies-5792e.firebaseapp.com",
-    databaseURL: "https://my-movies-5792e.firebaseio.com",
-    projectId: "my-movies-5792e",
-    storageBucket: "my-movies-5792e.appspot.com",
-    messagingSenderId: "694551446389",
-    appId: "1:694551446389:web:e34fab5e40eb72d73b093c",
-    measurementId: "G-HVGQQ92SXJ"
+    
   }
 };
 
